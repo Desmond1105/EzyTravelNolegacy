22520b98611307eed4a47a7a17575c2c0ea42ac6
Desmond1105 <rivalgyc@gmail.com>
2022-08-31 14:06:05 +0800

    ui

--- app/src/main/java/my/edu/utar/ezytravelnolegacy/MainActivity.java
+++ app/src/main/java/my/edu/utar/ezytravelnolegacy/MainActivity.java
@@ -1,36 +1,54 @@
 package my.edu.utar.ezytravelnolegacy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import my.edu.utar.ezytravelnolegacy.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new PlannerFragment());
 
+        binding.btmNgvBar.setOnItemSelectedListener(item ->{
 
-
-        DAOEmployee dao = new DAOEmployee();
-        EditText et = new EditText(this);
-        et = findViewById(R.id.testing_et);
-
-        Button btn = (Button) findViewById(R.id.btn_1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText et = new EditText(MainActivity.this);
-                et = findViewById(R.id.testing_et);
-
-                dao.add(et.getText().toString());
+            switch(item.getItemId()){
+                case R.id.nav_account:
+                    replaceFragment((new AccountFragment()));
+                    break;
+                case R.id.nav_spending:
+                    replaceFragment(new SpendingFragment());
+                    break;
+                case R.id.nav_checklist:
+                    replaceFragment(new ChecklistFragment());
+                    break;
+                case R.id.nav_planner:
+                    replaceFragment(new PlannerFragment());
+                    break;
             }
+
+            return true;
         });
 
+    }
 
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
